e7ee4553bff71b67bc52aa8356a838a7ffdd30a3
pishwithflaco <134299719+pishwithflaco@users.noreply.github.com>
2023-05-22 19:51:47 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+# creanenotes
---
+++ func_call.js
@@ -0,0 +1,15 @@
+function notes(){
+	window.open("notes/notes.html")
+}
+function homeWork(){
+	window.open("homework/homework.html")
+}
+function syllabus(){
+	window.open("syllabus.pdf")
+}
+function question(){
+	window.open("questionpaper/questionpaper.html")
+}
+function contactus(){
+	window.open("contact/index.html")
+}
